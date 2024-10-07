64ca6e45299ea287c1eee3e2ae007d29e9c449c6
Eduardo Henrique <eduardortodontia@gmail.com>
2024-10-07 10:40:56 -0300

    feat: :sparkles: add pt-br to admin language options

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,9 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    i18n: {
+      enabled: true,
+      config: {
+        defaultLocale: 'pt-BR',
+        locales: ['pt-BR', 'en'], // Adicione outros idiomas conforme necessário
+      },
+    },
+  });
